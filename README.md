80079a58195b796dbd05b006917f604a7cc5c9d2
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2019-02-23 20:03:35 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# slim-loadingscreen
+Slim-loadingscreen
+
+by Mackgame4 (discord.io/mack)
+
+```
+Intalling:
+Put loadingscreen in resources folder
+
+Put start in your server.cfg
+```
---
+++ js/config.js
@@ -0,0 +1,20 @@
+/* edit chang7 - EliteBR // edit: amorim#0567 synn screen */
+
+var config =
+{    
+
+    cursorImage: "img/cursor.png",
+
+    imageSize: [578, 236],
+    
+    music:
+    [
+        "FmZisnTvN_M",
+        "gR0AittLTAw",
+        "95LUtKZFRVs",
+    ],
+
+    enableMusic: true,
+ 
+    musicVolume: 30,
+}
